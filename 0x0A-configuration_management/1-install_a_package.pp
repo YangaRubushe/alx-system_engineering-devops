def6d59b53a12ceb0cdd68ca9417b2e5ed25ceec
Yanga Rubushe <zilithemba10@gmail.com>
2024-03-24 16:34:29 -0700

    Installs a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# installs puppet-lint package
-
-package {'flask':
+# install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+  provider => 'pip3',
 }
